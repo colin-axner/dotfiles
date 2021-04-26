c179b75e90540cfb6e3d9572b1c4679d3ff6e810
Colin Axnér <25233464+colin-axner@users.noreply.github.com>
2021-04-26 19:16:40 +0200

    update vim setup script

--- setup_vim.sh
+++ setup_vim.sh
@@ -2,4 +2,19 @@
 
 # I use this to add fancy stuff when I use a fresh cloud instance
 
+# colorscheme
+git clone https://github.com/sainnhe/everforest.git
+cp everforest/autoload/everforest.vim ~/.vim/autoload/
+cp everforest/colors/everforest.vim ~/.vim/colors/everforest.vim
+cp everforest/doc/everforest.txt ~/.vim/doc/
+cp everforest/autoload/airline/themes/everforest.vim ~/.vim/autoload/airline/themes/
+
+# go plugin
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+
+# airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+
+echo "run :helptags ~/.vim/pack/dist/start/vim-airline/doc"
+
+
